5f0adc6c6e4ea10734807352b8e8f41aa45c77a9
nicole wachter <nicole.wachter6@gmail.com>
2024-06-18 11:40:56 -0400

    Adds index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,69 @@
+
 // index.js
-
-// Callbacks
+const ramenMenu = document.getElementById("ramen-menu");
+let detailImage = document.getElementById("detail-image");
+let nameD = document.getElementById("name-d");
+let restaurant= document.getElementById("restaurant-d");
+let rating = document.getElementById("rating-display");
+let comment = document.getElementById("comment-display");
+// Callback
 const handleClick = (ramen) => {
-  // Add code
-};
+  detailImage.src = ramen.image;
+  nameD.textContent = ramen.name;
+  restaurant.textContent = ramen.restaurant;
+  rating.textContent = ramen.rating;
+  comment.textContent = ramen.comment;
 
-const addSubmitListener = () => {
-  // Add code
+}
+   // Add code
+ const addSubmitListener = () => {
+  document.querySelector("#new-ramen").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const newObject = {
+    name: event.target.name.value,
+    restaurant: event.target.restaurant.value,
+    image: event.target.image.value,
+    rating: event.target.rating.value,
+    comment: event.target["new-comment"].value,
+  };
+  addImage(newObject);
+  event.target.reset();
+
+ });
+};
+//Grab the ramen menu
+//GET request for ramen bar
+function displayRamens() {
+  fetch("http://localhost:3000/ramens")
+  .then(response => response.json())
+  .then(ramenR => newImages(ramenR));
+  
+ }
+// append images to 
+function newImages (imgRamen) {
+  imgRamen.forEach((ramen) => addImage(ramen));
+  }
+  
+function addImage (imgUrl){
+  const ramenIm = document.createElement("img");
+  ramenIm.src = imgUrl.image;
+  ramenMenu.append(ramenIm);
+  ramenIm.addEventListener("click", () => handleClick(imgUrl));
 }
 
-const displayRamens = () => {
-  // Add code
-};
 
+ 
 const main = () => {
+  displayRamens();
+  addSubmitListener();
+  // handleClick;
+
   // Invoke displayRamens here
   // Invoke addSubmitListener here
 }
 
-main()
+main();
 
 // Export functions for testing
 export {
